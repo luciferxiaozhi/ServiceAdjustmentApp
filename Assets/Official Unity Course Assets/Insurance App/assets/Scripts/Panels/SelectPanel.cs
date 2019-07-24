bfecb934f63138205841addb646a43fae51e38a3
luciferxiaozhi <luciferxiaozhi@163.com>
2019-07-24 14:16:49 -0400

    Initial Commit

---
+++ Assets/Official Unity Course Assets/Insurance App/assets/Scripts/Panels/SelectPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectPanel : MonoBehaviour, IPanel
+{
+    public Text informationText;
+
+    public void OnEnable()
+    {
+        UIManager.Instance.activePanels.Push(this.gameObject);
+        informationText.text = UIManager.Instance.activeCase.name;
+    }
+    public void ProcessInfo()
+    {
+        
+    }
+}
---
+++ Assets/Official Unity Course Assets/Insurance App/assets/Scripts/UIManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEditor;
+using Random = UnityEngine.Random;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("Error: The UIManager is null!");
+            }
+            return _instance;
+        }
+    }
+
+    public Case activeCase;
+    public ClientInfoPanel clientInfoPanel;
+    public GameObject borderPanel;
+    public Stack<GameObject> activePanels;
+
+    private void OnEnable()
+    {
+        activePanels = new Stack<GameObject>();
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void CreateNewCase()
+    {
+        activeCase = new Case();
+        
+
+        // generate a caseID
+        string randNumber = Random.Range(0, 1000).ToString();
+//        while (AWSManager.Instance.IsExistCaseOnS3(randNumber).Result)
+//        {
+//            randNumber = Random.Range(0, 1000).ToString();
+//        }
+        
+        activeCase.caseID = "" + randNumber;
+
+        clientInfoPanel.gameObject.SetActive(true);
+        borderPanel.SetActive(true);
+    }
+
+    public void SubmitButton()
+    {
+        // create a new case to save
+        // populate the case data
+        // open a data stream to turn that object(file) into a file
+
+        Case awsCase = DeepCloneCase(activeCase);
+
+        string filePath = Application.persistentDataPath + "/case#" + awsCase.caseID + ".dat";
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(filePath);
+        bf.Serialize(file, awsCase);
+        file.Close();
+
+        Debug.Log("Application Data Path: " + Application.persistentDataPath);
+
+        // send to aws
+        AWSManager.Instance.UploadToS3(filePath, awsCase.caseID);
+
+    }
+
+    public Case DeepCloneCase(Case targetCase)
+    {
+        Case newCase = new Case();
+        newCase.caseID = targetCase.caseID;
+        newCase.name = targetCase.name;
+        newCase.date = targetCase.date;
+        newCase.locationNotes = targetCase.locationNotes;
+        newCase.map = targetCase.map;
+        newCase.photoTaken = targetCase.photoTaken;
+        newCase.photoNotes = targetCase.photoNotes;
+
+        return newCase;
+    }
+}
