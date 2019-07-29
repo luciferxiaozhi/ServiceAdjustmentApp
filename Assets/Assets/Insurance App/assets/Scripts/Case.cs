bb5e233855f3cd697a9f154a05daeaef8f1b68be
luciferxiaozhi <luciferxiaozhi@163.com>
2019-07-29 13:55:02 -0400

    Add README, deleted unrelated assets.

---
+++ Assets/Assets/Insurance App/assets/Scripts/Case.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable] // allow to see this class in inspector 
+public class Case
+{
+    public string caseID;
+    public string name;
+    public string date;
+    public byte[] map;
+    public string locationNotes;
+    public byte[] photoTaken;
+    public string photoNotes;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Service Adjustment App - Rainy Day Insurance
+
+- Developed a mobile app that allows user to create and submit a case, and search for case information from cloud.
+- Implemented location map functionality by integrating GPS location services (Google map) into the app.
+- Stored all form data in the AWS cloud using S3 Storage.
+
+**See overview of the app in the PDFs folder.**
